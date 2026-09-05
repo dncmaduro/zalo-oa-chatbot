2fa5809062d8e33e06bf21806b13894c328cc2cd
dncmaduro <manhdungtran.vnuuet@gmail.com>
2026-09-05 16:37:19 +0700

    update file name

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/primsa.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { MediaStorageModule } from './media-storage/media-storage.module';
 import { KnowledgeImportModule } from './modules/knowledge-import/knowledge-import.module';
 import { KnowledgeModule } from './modules/knowledge/knowledge.module';
